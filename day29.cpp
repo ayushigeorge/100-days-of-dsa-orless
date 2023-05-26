cb71a795ff6d50f562c93ac227c99d67bfeb2676
Ayushi George <ayushigeorge31@gmail.com>
2023-05-26 12:04:04 +0530

    Create day29.cpp

---
+++ day29.cpp
@@ -0,0 +1,11 @@
+int minValue(Node* root) {
+    // Code here
+    if(root==NULL){
+        return -1;
+        
+    }
+    if(root-> left==NULL){
+        return root->data;
+    }
+    return minValue(root->left);
+}
