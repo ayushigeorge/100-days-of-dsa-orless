8cc30237d6671588ed12e29ecd0459f8563c2740
Ayushi George <ayushigeorge31@gmail.com>
2023-07-06 15:57:25 +0530

    Create day51.cpp

---
+++ day51.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set <int>i;
+        for(int n=0; n<nums.size();n++){
+            i.insert(nums[n]);
+        }
+        int ans= i.size();
+        nums.clear();
+        for(auto n:i){
+            nums.push_back(n);
+        }
+        return ans;
+    }
+};
