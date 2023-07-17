484bde3bc3417a3c0cc13cb28fd391d06e8fa560
Ayushi George <ayushigeorge31@gmail.com>
2023-07-17 13:31:41 +0530

    Create day62.cpp

---
+++ day62.cpp
@@ -0,0 +1,26 @@
+
+class Solution{
+public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    unordered_map<char, int>mp;
+		    queue<char>q;
+		    string ans="";
+		    for(char c:A){
+		        mp[c]++;
+		        if(mp[c]==1){
+		            q.push(c);
+		        }
+		        while(!q.empty()&& mp[q.front()]>1){
+		            q.pop();
+		        }
+		        if(q.empty()){
+		            ans+='#';
+		        }
+		        else {
+		            ans+=q.front();
+		        }
+		    }
+		    return ans;
+    }   
+		};
